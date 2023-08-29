9bb1fe9013199135d928a25a72d80bcf46c2cc1e
Lucas Bateson <lucas.bateson@gmail.com>
2023-08-29 22:32:33 +0200

    Oppgave 1 oppdatering

--- oppgave_1/index.js
+++ oppgave_1/index.js
@@ -1,99 +1,33 @@
 var dag = new Date();
+
 var dager = ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'];
 var dagNavn = dager[dag.getDay()];
+
 var dato = dag.getDate();
-var måned = dag.getMonth();
+var måned = dag.getMonth() + 1;
 var år = dag.getFullYear();
-var klokke = dag.toLocaleTimeString();
-var klokkeTall = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeSekund = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeMinutt = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeTime = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeSekund = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeMinutt = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeTime = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeSekund = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeMinutt = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeTime = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeSekund = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeMinutt = dag.toLocaleTimeString('no-NO', {
-  hour12: false,
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric"
-});
-var klokkeTime = dag.toLocaleTimeString('no-NO', {
+
+var klokke = dag.toLocaleTimeString('no-NO', {
   hour12: false,
   hour: "numeric",
   minute: "numeric",
   second: "numeric"
 });
 
-document.getElementById('dato').innerHTML = dagNavn + ' ' + dato + '.' + måned + '.' + år;
-
+document.getElementById('dato').innerHTML = `${dagNavn} ${dato}.${måned}.${år}`;
 document.getElementById('klokke').innerHTML = klokke;
 
-if (dagNavn == 'Søndag') {
+if (dagNavn === 'Søndag') {
   document.getElementById('dato').className = 'red';
 }
 
 setInterval(function () {
   dag = new Date();
-  klokke = dag.toLocaleTimeString();
+  klokke = dag.toLocaleTimeString('no-NO', {
+    hour12: false,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+  });
   document.getElementById('klokke').innerHTML = klokke;
 }, 1000);
